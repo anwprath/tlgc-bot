487dcc36e1e5070d82ff9beca3267fc169cf4232
Prathamesh Anwekar <pratham.2118@gmail.com>
2026-07-18 15:32:36 +0530

    Updated httpw client init

--- src/httpw.rs
+++ src/httpw.rs
@@ -1,10 +1,25 @@
-use reqwest::Client;
+use std::{sync::OnceLock, time::Duration};
+
+use reqwest::{Client, Response};
 use serde_json::Value;
 
+const REQUEST_TIMEOUT_SECS: u64 = 10;
+static HTTP_CLIENT: OnceLock<Client> = OnceLock::new();
+
 pub async fn post(url: &str, payload: Value) -> Result<Value, Box<dyn std::error::Error>> {
-    let client = Client::new();
+    let response: Response = client()
+        .post(url)
+        .json(&payload)
+        .timeout(Duration::from_secs(REQUEST_TIMEOUT_SECS))
+        .send()
+        .await?
+        .error_for_status()?;
 
-    let response: Value = client.post(url).json(&payload).send().await?.json().await?;
+    let json: Value = response.json().await?;
 
-    Ok(response)
+    Ok(json)
+}
+
+fn client() -> &'static Client {
+    return HTTP_CLIENT.get_or_init(Client::new);
 }
